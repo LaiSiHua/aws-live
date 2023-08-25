861b83bfef22228c3a39cee0d5b25b0967f78a80
LaiSiHua <87710128+LaiSiHua@users.noreply.github.com>
2023-08-26 00:18:04 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.clwe6mkxcp4t.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "laisihua-employee"
+customregion = "us-east-1"
+
